77ce0ba362141a22a17359e67b0909c531503d28
Lorena <Lorenamoujalli860@gmail.com>
2025-10-16 13:04:57 -0400

    terminando la estructura de la pagina

---
+++ public/scripts/preguntas_frecuentes.js
@@ -0,0 +1,16 @@
+var faqItems = document.querySelectorAll(".faq-item");
+
+  for (var i = 0; i < faqItems.length; i++) {
+    faqItems[i].querySelector(".faq-question").addEventListener("click", function () {
+      var item = this.parentNode;
+
+      // Alternar la clase 'active'
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+      } else {
+        item.classList.add("active");
+      }
+    });
+  }
+
+  
