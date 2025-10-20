2d2a5e16019f3cf0377ff63a25f23491c21e9459
Lorena <Lorenamoujalli860@gmail.com>
2025-10-20 13:40:19 -0400

    terminando estilos

--- public/scripts/preguntas_frecuentes.js
+++ public/scripts/preguntas_frecuentes.js
@@ -1,4 +1,4 @@
-var faqItems = document.querySelectorAll(".faq-item");
+ var faqItems = document.querySelectorAll(".faq-item"); 
 
   for (var i = 0; i < faqItems.length; i++) {
     faqItems[i].querySelector(".faq-question").addEventListener("click", function () {
@@ -13,4 +13,20 @@ var faqItems = document.querySelectorAll(".faq-item");
     });
   }
 
-    
+
+/*
+   var faqItems2 = document.querySelectorAll(".faq-item2"); 
+
+  for (var i = 0; i < faqItems2.length; i++) {
+    faqItems2[i].querySelector(".faq-question2").addEventListener("click", function () {
+      var item = this.parentNode;
+
+      // Alternar la clase 'active'
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+      } else {
+        item.classList.add("active");
+      }
+    });
+  }
+    */
