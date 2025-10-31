22d4917f83d089efb187385044f86a9b533cad90
Candela Villaverde <candela@oberstaff.com>
2025-10-31 17:49:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import astro from '@astrojs/vite-plugin-astro';
 
+// Permite dominios externos (como los de Coolify *.sslip.io)
 export default defineConfig({
   plugins: [astro()],
   preview: {
-    allowedHosts: ['hoks8wc8sw8c8skksg84wocw.109.199.104.87.sslip.io'],
     host: '0.0.0.0',
     port: 3000,
+    allowedHosts: [
+      'hoks8wc8sw8c8skksg84wocw.109.199.104.87.sslip.io',
+      '.sslip.io' // permite cualquier subdominio sslip.io
+    ],
   },
 });
