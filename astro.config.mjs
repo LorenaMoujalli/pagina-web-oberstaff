cadf1f584291abf23434f30adbc70575ce90490f
candela-oberstaff <candela@oberstaff.com>
2025-11-07 15:24:22 -0300

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    preview: {
+      allowedHosts: ['proto.oberstaff.com', 'localhost', 'oberstaff.com'],
+    },
+  },
 });
