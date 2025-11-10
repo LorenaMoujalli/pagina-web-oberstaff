303133c95bc280cbcbd9a4a4eda2c1632b313060
candela-oberstaff <candela@oberstaff.com>
2025-11-10 10:33:37 -0300

    config deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel'
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [react()],
+  adapter: node({
+    mode: 'standalone', // ✅ necesario para correr en Coolify/Docker
+  }),
   vite: {
     server: {
       allowedHosts: ['proto.oberstaff.com', '.sslip.io', 'localhost'],
@@ -12,7 +15,5 @@ export default defineConfig({
     preview: {
       allowedHosts: ['proto.oberstaff.com', '.sslip.io', 'localhost'],
     },
-    
   },
-  adapter: vercel(),
 });
